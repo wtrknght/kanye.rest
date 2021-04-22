703c48b7d8e6aef9787da2519d9a3760637c3117
ajzbc <contact.ajzbc@gmail.com>
2021-04-21 23:03:50 -0700

    redesign

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+    import Quote from "./Quote.svelte";
+</script>
+
+<main>
+    <div class="wrapper">
+        <div class="container header">
+            <h2><a href="./">🌊 kanye.rest</a></h2>
+            <span>A free REST API for random Kanye West quotes (Kanye as a Service)</span>
+        </div>
+
+        <div class="container">
+            <Quote />
+        </div>
+
+        <div class="container ad">
+            <script
+                async
+                type="text/javascript"
+                src="//cdn.carbonads.com/carbon.js?serve=CE7D5K7M&placement=kanyerest"
+                id="_carbonads_js">
+            </script>
+        </div>
+
+        <div class="container footer">
+            <p>
+                👋
+                <a href="https://twitter.com/ajzbc">@ajzbc</a>
+            </p>
+            <p>
+                <a href="https://github.com/ajzbc/kanye.rest">
+                    <img
+                        src="https://img.shields.io/github/stars/ajzbc/kanye.rest?style=social"
+                        alt="Github Link"
+                    />
+                </a>
+            </p>
+        </div>
+    </div>
+</main>
+
+<style>
+    .wrapper {
+        margin: 0 auto;
+        max-width: 550px;
+    }
+
+    .container {
+        margin-bottom: 2rem;
+    }
+
+    .header {
+        text-align: center;
+    }
+
+    .ad {
+        min-height: 100px;
+    }
+
+    .footer {
+        text-align: center;
+    }
+
+    h2 {
+        margin: 0.8rem 0;
+    }
+
+    h2 a,
+    h2 a:hover {
+        color: var(--main-text);
+        text-decoration: none;
+    }
+
+    span {
+        color: var(--sub-text);
+        font-size: 0.8rem;
+    }
+
+    p {
+        margin: 0.75rem 0;
+    }
+</style>
---
+++ src/Quote.svelte
@@ -0,0 +1,94 @@
+<script>
+    import { fly } from "svelte/transition";
+
+    async function getQuote() {
+        const response = await fetch("https://api.kanye.rest");
+        const text = (await response.json()).quote;
+        tweet_url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            text + " -https://kanye.rest"
+        )}`;
+        return text;
+    }
+
+    let quote = getQuote();
+
+    function refresh() {
+        quote = getQuote();
+    }
+
+    let tweet_url;
+    function tweet() {
+        window.open(tweet_url);
+    }
+</script>
+
+<main>
+    <div class="top">
+        <a href="https://api.kanye.rest">https://api.kanye.rest</a>
+
+        <button on:click={tweet}>📢 tweet</button>
+        <button on:click={refresh}>🔄 refresh</button>
+    </div>
+
+    <div class="quote">
+        {#await quote}
+            <p>loading...</p>
+        {:then data}
+            <p in:fly={{ x: -100, duration: 150 }}>
+                "{data}"
+            </p>
+        {/await}
+        <span> - Kanye West</span>
+    </div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .top {
+        display: flex;
+        align-items: center;
+        padding: 0.75rem 1rem;
+    }
+
+    a {
+        margin-right: auto;
+    }
+
+    button {
+        margin-left: 0.5rem;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        background-color: transparent;
+        font-family: inherit;
+        font-size: 0.8rem;
+    }
+
+    .quote {
+        padding: 1rem;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        background-color: hsl(0, 0%, 96%);
+        border-radius: 10px;
+        position: relative;
+        display: block;
+        overflow: hidden;
+    }
+
+    p {
+        margin: 0.5rem 0;
+    }
+
+    span {
+        padding: 6px 8px;
+        font-size: 0.8rem;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        color: var(--sub-text);
+        border-radius: 0 0 10px 0;
+    }
+</style>
