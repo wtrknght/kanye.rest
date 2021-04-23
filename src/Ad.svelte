13d31061329403aa8cfc5e19bb1e31307940c7e5
ajzbc <contact.ajzbc@gmail.com>
2021-04-22 19:34:59 -0700

    Moved ad to its own file

---
+++ src/Ad.svelte
@@ -0,0 +1,21 @@
+<script>
+    /* TODO: show text when ad blocked*/
+</script>
+
+<main>
+    <div class="card">
+        <script
+            async
+            type="text/javascript"
+            src="https://cdn.carbonads.com/carbon.js?serve=CE7D5K7M&placement=kanyerest"
+            id="_carbonads_js">
+        </script>
+    </div>
+</main>
+
+<style>
+    div {
+        padding: 0;
+        /* min-height: 100px; */
+    }
+</style>
